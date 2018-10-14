501637d686ff68f6eabcf8e8777a544c7ba5ccb4
JoannaWB <joanna.walkosz@gmail.com>
2018-10-14 11:37:11 +0200

    change error message

--- src/main/java/ekindergarten/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/ekindergarten/security/jwt/JwtAuthenticationEntryPoint.java
@@ -15,6 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
         httpServletResponse
-                .sendError(HttpServletResponse.SC_FORBIDDEN, "Wypierdalać");
+                .sendError(HttpServletResponse.SC_FORBIDDEN, "Error");
     }
 }
